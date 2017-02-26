6f31025048c9eeae7b0765a0e41c19b847090937
System <none>
2017-02-26 10:20:04 +0000

    🌔🦄 Updated with Gomix

---
+++ public/client.js
@@ -0,0 +1,17 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+
+$(function() {
+  /*$('form').submit(function(event) {
+    event.preventDefault();
+    var dream = $('input').val();
+    $.post('/dreams?' + $.param({dream: dream}), function() {
+      $('<li></li>').text(dream).appendTo('ul#dreams');
+      $('input').val('');
+      $('input').focus();
+    });
+  });*/
+});
